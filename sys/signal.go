dda73f0bafb9174c2dcbf7a79f5864e5515ee886
Seaprince <admin@eaxi.com>
2012-09-17 16:24:31 +0800

    signal handling

--- sys/signal.go
+++ sys/signal.go
@@ -8,13 +8,20 @@ import (
 )
 
 func main() {
-
+    sigFlag := 0
     go func() {
         c := make(chan os.Signal, 1)
         signal.Notify(c, os.Interrupt)
         for {
             sig := <- c
-            fmt.Printf("CTRL-C: %v\n", sig)
+            sigFlag ++
+            if sigFlag > 1 {
+                os.Exit(0)
+            }
+            fmt.Printf("CTRL-C: %v, once more within 1s to exit.\n", sig)
+            time.AfterFunc(1e9, func() {
+                sigFlag = 0
+            })
         }
     }()
 
